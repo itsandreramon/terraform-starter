4f5b081bcfb0afef8484b57be24dd463bb347e99
andrethiele <thielea@th-brandenburg.de>
2022-01-21 17:50:24 +0100

    remove Testcontainers for now

--- src/test/kotlin/app/spring/AppTests.kt
+++ src/test/kotlin/app/spring/AppTests.kt
@@ -7,17 +7,12 @@ import app.spring.graphql.types.BookInput
 import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest
 import com.netflix.graphql.dgs.reactive.DgsReactiveQueryExecutor
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.context.annotation.ComponentScan
-import org.testcontainers.containers.GenericContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.utility.DockerImageName
 import reactor.test.StepVerifier
 
 @ComponentScan
@@ -32,24 +27,6 @@ import reactor.test.StepVerifier
 class AppTests {
 
     @Autowired lateinit var queryExecutor: DgsReactiveQueryExecutor
-
-    @Container
-    var mongo: GenericContainer<*> = GenericContainer(
-        DockerImageName.parse("mongo")
-    ).withExposedPorts(27017).apply {
-        addEnv("MONGO_INITDB_ROOT_USERNAME", "root")
-        addEnv("MONGO_INITDB_ROOT_PASSWORD", "example")
-    }
-
-    @BeforeEach
-    fun setUp() {
-        mongo.start()
-    }
-
-    @AfterEach
-    fun tearDown() {
-        mongo.stop()
-    }
 
     @Test
     fun test_save_book() {
