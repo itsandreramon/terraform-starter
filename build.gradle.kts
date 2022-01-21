a8123c66566525c168c13d943d825b6d27c5b34d
andrethiele <thielea@th-brandenburg.de>
2022-01-21 14:37:45 +0100

    use version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 import com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-    id("org.springframework.boot") version "3.0.0-M1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.springframework.boot") version "3.0.0-M1"
     id("com.netflix.dgs.codegen") version "5.1.16"
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
+    kotlin("plugin.serialization") version "1.6.0"
 }
 
 repositories {
@@ -14,33 +16,22 @@ repositories {
     maven { url = uri("https://repo.spring.io/milestone") }
 }
 
-group = "app"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
-extra["testcontainersVersion"] = "1.16.2"
-extra["dgsVersion"] = "4.9.16"
-
 dependencies {
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("com.netflix.graphql.dgs:graphql-dgs-webflux-starter")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:mongodb")
-}
+    implementation(platform(libs.netflix.dgs.bom))
+    implementation(libs.netflix.dgs.starter)
 
-dependencyManagement {
-    imports {
-        mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
-        mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:${property("dgsVersion")}")
-    }
+    implementation(libs.spring.mongo)
+    implementation(libs.spring.webflux)
+
+    implementation(libs.reactor.kotlin)
+    implementation(libs.kotlin.stdlib)
+
+    testImplementation(platform(libs.testcontainers.bom))
+    testImplementation(libs.testcontainers.junit)
+    testImplementation(libs.testcontainers.mongo)
+
+    testImplementation(libs.reactor.test)
+    testImplementation(libs.spring.test)
 }
 
 tasks.withType<KotlinCompile> {
@@ -48,7 +39,7 @@ tasks.withType<KotlinCompile> {
 
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 
@@ -56,8 +47,16 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+tasks.withType<BootBuildImage> {
+    imageName = "spring:latest"
+}
+
 tasks.withType<GenerateJavaTask> {
     schemaPaths = mutableListOf("${projectDir}/src/main/resources/schema")
     packageName = "app.spring.graphql"
     generateClient = true
 }
+
+group = "app"
+version = "-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,4 +4,16 @@ pluginManagement {
 		gradlePluginPortal()
 	}
 }
-rootProject.name = "spring"
+
+plugins {
+	id("com.gradle.enterprise") version "3.8.1"
+}
+
+gradleEnterprise {
+	buildScan {
+		termsOfServiceUrl = "https://gradle.com/terms-of-service"
+		termsOfServiceAgree = "yes"
+	}
+}
+
+enableFeaturePreview("VERSION_CATALOGS")
