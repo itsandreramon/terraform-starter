81554608b6f21c719d743d82fc2b825db065a80b
andrethiele <thielea@th-brandenburg.de>
2022-02-04 17:42:23 +0100

    fix path

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
-APP_DIR=terraform/app/
+APP_DIR=terraform/app
 
 terraform -chdir=$APP_DIR/db init &&
   terraform -chdir=$APP_DIR/db apply -auto-approve
 
 terraform -chdir=$APP_DIR/spring init &&
-  terraform -chdir=$APP_DIR/spring apply -auto-approve
+  terraform -chdir=$APP_DIR/spring apply $1 -auto-approve
--- destroy-app.sh
+++ destroy-app.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-APP_DIR=terraform/app/
+APP_DIR=terraform/app
 terraform -chdir=$APP_DIR/spring destroy -auto-approve
 terraform -chdir=$APP_DIR/db destroy -auto-approve
