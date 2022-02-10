d0772377630b5e232caf83f14ebf33ed17749f1d
andrethiele <thielea@th-brandenburg.de>
2022-02-10 20:25:02 +0100

    set utf-8 as default for MySQL

--- terraform/app/db/main.tf
+++ terraform/app/db/main.tf
@@ -2,19 +2,40 @@ provider "aws" {
   region = "us-east-2"
 }
 
+locals {
+  mysql_version = "8.0"
+}
+
 resource "aws_db_instance" "db" {
   identifier_prefix      = "db-"
   name                   = "db"
   allocated_storage      = "10"
-  engine                 = "mysql"
   instance_class         = "db.t2.micro"
+  engine                 = "mysql"
+  engine_version         = local.mysql_version
   vpc_security_group_ids = [aws_security_group.db.id]
+  parameter_group_name   = aws_db_parameter_group.db.name
 
   publicly_accessible = true
   skip_final_snapshot = true
 
   username = "root"
   password = "password"
+}
+
+resource "aws_db_parameter_group" "db" {
+  name   = "db"
+  family = "mysql${local.mysql_version}"
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8"
+  }
+
+  parameter {
+    name  = "character_set_client"
+    value = "utf8"
+  }
 }
 
 resource "aws_security_group" "db" {
