56b34c5f0bac78cf90002edb646a6467c4ac2ae0
andrethiele <thielea@th-brandenburg.de>
2022-02-11 22:30:42 +0100

    fix

--- deploy-app.sh
+++ deploy-app.sh
@@ -5,5 +5,5 @@ APP_DIR=terraform/app
 terraform -chdir=$APP_DIR/db init &&
   terraform -chdir=$APP_DIR/db apply -auto-approve
 
-terraform -chdir=$APP_DIR/db init &&
-  terraform -chdir=$APP_DIR/db apply -auto-approve
+terraform -chdir=$APP_DIR/instance init &&
+  terraform -chdir=$APP_DIR/instance apply -auto-approve
