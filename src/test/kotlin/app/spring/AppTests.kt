6ef6bb3d6302e4c2b641c2ac32ab7ad55b2a4ac6
andrethiele <thielea@th-brandenburg.de>
2022-02-11 20:03:46 +0100

    remove unused annotation

--- src/test/kotlin/app/spring/AppTests.kt
+++ src/test/kotlin/app/spring/AppTests.kt
@@ -1,35 +1,23 @@
 package app.spring
 
-import app.spring.fetcher.BookFetcher
 import app.spring.graphql.client.SaveBookGraphQLQuery
 import app.spring.graphql.client.SaveBookProjectionRoot
 import app.spring.graphql.types.BookInput
 import com.netflix.graphql.dgs.DgsQueryExecutor
-import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
-@ComponentScan
-@EnableAutoConfiguration
-@SpringBootTest(
-    webEnvironment = RANDOM_PORT,
-    classes = [
-        DgsAutoConfiguration::class,
-        BookFetcher::class,
-    ]
-)
 @Testcontainers
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class AppTests {
 
     @Autowired lateinit var queryExecutor: DgsQueryExecutor
