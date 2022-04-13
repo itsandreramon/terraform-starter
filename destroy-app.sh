4cfc3943ff7d5053949bdb24754284e5de0d67ed
andrethiele <thielea@th-brandenburg.de>
2022-04-13 12:08:45 +0200

    fix destroy script

--- destroy-app.sh
+++ destroy-app.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 APP_DIR=terraform/app
-terraform -chdir=$APP_DIR/spring destroy -auto-approve
+terraform -chdir=$APP_DIR/instance destroy -auto-approve
 terraform -chdir=$APP_DIR/db destroy -auto-approve
+terraform -chdir=$APP_DIR/vpc destroy -auto-approve
