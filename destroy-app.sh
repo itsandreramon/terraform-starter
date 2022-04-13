69d735f1b2fd4ae475ee1dd3ade32770b7f60c1c
andrethiele <thielea@th-brandenburg.de>
2022-04-13 12:13:50 +0200

    read variables in destroy script

--- destroy-app.sh
+++ destroy-app.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
+export $(xargs <.env)
 APP_DIR=terraform/app
+
 terraform -chdir=$APP_DIR/instance destroy -auto-approve
 terraform -chdir=$APP_DIR/db destroy -auto-approve
 terraform -chdir=$APP_DIR/vpc destroy -auto-approve
